1cc3d1ef1253de98696f7d0aa751bbf178527dc9
Professional <new99kpicem@gmail.com>
2023-07-15 23:15:26 +0300

    work

---
+++ src/main/java/com/example/course2/exception/QuestionNotFoundException.java
@@ -0,0 +1,27 @@
+package com.example.course2.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class QuestionNotFoundException extends RuntimeException {
+    public QuestionNotFoundException() {
+        super();
+    }
+
+    public QuestionNotFoundException(String message) {
+        super(message);
+    }
+
+    public QuestionNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public QuestionNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    protected QuestionNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
---
+++ src/main/java/com/example/course2/service/Impl/JavaQuestionService.java
@@ -0,0 +1,51 @@
+package com.example.course2.service.Impl;
+
+import com.example.course2.exception.QuestionAlreadyExistsException;
+import com.example.course2.exception.QuestionNotFoundException;
+import com.example.course2.model.Question;
+import com.example.course2.service.QuestionService;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class JavaQuestionService implements QuestionService {
+    public static final Random RANDOM = new Random();
+    List<Question> questions = new ArrayList<>();
+    @Override
+    public Question add(String question, String answer) {
+        return add(new Question(question, answer));
+    }
+
+    @Override
+    public Question add(Question question) {
+        if (questions.contains(question)) {
+            throw new QuestionAlreadyExistsException();
+        }
+        questions.add(question);
+        return question;
+    }
+
+    @Override
+    public Question remove(String question, String answer) {
+        return remove(new Question(question, answer));
+    }
+
+    @Override
+    public Question remove(Question question) {
+        if (questions.remove(question)) {
+            return question;
+        }
+        throw new QuestionNotFoundException();
+    }
+
+    @Override
+    public Collection<Question> getAll() {
+        return Collections.unmodifiableCollection(questions);
+    }
+
+    @Override
+    public Question getRandomQuestion() {
+        return questions.get(RANDOM.nextInt(questions.size()));
+    }
+}
